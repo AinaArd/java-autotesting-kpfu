bc5eea8f2f3d29cc601c8f2466698a109dbb6496
Aina <ainaard@mail.ru>
2019-11-20 10:43:16 +0300

    delete concrete education

--- Autotesting/src/test/java/tests/Test2.java
+++ Autotesting/src/test/java/tests/Test2.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 
 public class Test2 extends TestBase {
     private Account account = new Account("ARArdashirova", "u2hkip8c");
-    private Education education = new Education("12.11.2018", "13.11.2019", "TEST");
+//    private Education education = new Education("12.11.2018", "13.11.2019", "TEST");
+    private final String deleteElement = "УГАТУ";
 
     @Test
     public void deleteElem() throws Exception {
@@ -15,10 +16,8 @@ public class Test2 extends TestBase {
         applicationManager.getNavigationHelper().openProfilePage();
 
         applicationManager.getNavigationHelper().openEducation();
-        applicationManager.getEducationHelper().addEducation(education);
-//        applicationManager.getEducationHelper().testCheckExist();
 
-        applicationManager.getEducationHelper().deleteEducation(education.getOrganization());
+        applicationManager.getEducationHelper().deleteEducation(deleteElement);
 
         applicationManager.getLoginHelper().logout();
 
